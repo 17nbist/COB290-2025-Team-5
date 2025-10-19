a56685264f86c86a82445d8683029fdbc55738b1
17nbist <17nbist@gmail.com>
2025-10-19 10:44:07 +0100

    [nb]fixed merge conflicts

--- src/app/dashboard/DashboardPage.js
+++ src/app/dashboard/DashboardPage.js
@@ -4,21 +4,21 @@ import { useEffect } from 'react';
 
 
 export default function DashboardPage() {
-    useEffect(() => {
-      document.title = 'Dashboard | Make-It-All';
-    }, []);
-    return (
-        <div>
-        <ProfileOverview name={"John Doe"} position={"CEO"} taskAllocated={"12"} ProjectAllocated={"18"}/>
-        </div>
-    );
-}*/
+  useEffect(() => {
+    document.title = 'Dashboard | Make-It-All';
+  }, []);
+  return (
+    <div>
+      <ProfileOverview name={"John Doe"} position={"CEO"} taskAllocated={"12"} ProjectAllocated={"18"} />
+    </div>
+  );
+}
 
 //May need to change later, just testing if miniCalendar works. 
 "use client";
 
 import { useEffect } from "react";
-import MiniCalendar from "@/components/MiniCalendar"; 
+import MiniCalendar from "@/components/MiniCalendar";
 
 export default function DashboardPage() {
   useEffect(() => {
