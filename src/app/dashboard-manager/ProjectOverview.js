13acede13424b338b41200aceca347266a97887f
Joshua Bakata <joshuabakata@gmail.com>
2025-10-21 15:53:41 +0100

    added the foundational layer of the manager dashboard a bit of visual and updated the pie chart to feature in a random background color generator to have the pieChart more responsive to different input size.

--- src/app/dashboard-manager/ProjectOverview.js
+++ src/app/dashboard-manager/ProjectOverview.js
@@ -1,44 +1,51 @@
+import { progress } from "framer-motion"
 export default function ProjectOverview() {
     return (
-        <div className="table" style={{ height: "30vh", width: "30vh" }} >
-            <div className="table__title">Project Overview</div>
-            <div className="table_header">
-                <div className="item_header">Project</div>
-                <div className="item_header">Progress</div>
-                <div className="item_header">Status</div>
-            </div>
-            <div className="table__data">
-                {/* project */}
-                <div className="table__right">
-                    <div className="item">Website Design</div>
-                    <div className="item">Mobile App</div>
-                    <div className="item">odkeode</div>
+        // style={{ height: "40vh", width: "40vh" }}
+        <>
+            <div className="table"  >
+                <div className="table__title">Project Overview</div>
+                <div className="table_header">
+                    <div className="item_header">Project</div>
+                    <div className="item_header">Progress</div>
+                    <div className="item_header">Status</div>
                 </div>
-                {/* progress */}
-                <div className="table__right">
-                    <div className="item">||||___</div>
-                    <div className="item">|______</div>
-                    <div className="item">||||||_</div>
+                <div className="table__data">
+                    {/* project */}
+                    <div className="table__right">
+                        <div className="item">Website Design</div>
+                        <div className="item">Mobile App</div>
+                        <div className="item">odkeode</div>
+                    </div>
+                    {/* progress */}
+                    <div className="table__right">
+                        <div className="item"><progress value={0.6} style={{color:"blue"}} /></div>
+                        <div className="item"><progress value={0.2} /></div>
+                        <div className="item"><progress value={0.9} /></div>
+                    </div>
+                    {/* status */}
+                    <div className="table__right">
+                        <div className="item">On Track</div>
+                        <div className="item">Behind</div>
+                        <div className="item">Almost Completed</div>
+                    </div>
                 </div>
-                {/* status */}
-                <div className="table__right">
-                    <div className="item">On Track</div>
-                    <div className="item">Behind</div>
-                    <div className="item">Almost Completed</div>
+                <div className="table_header">
+                    <div className="item_header" style={{ display: "grid", gridTemplateRows: "auto auto", justifyContent: "center", alignItems: "center" }}>
+                        <p>45</p><p>Total Task</p>
+                    </div>
+                    <div className="item_header" style={{ display: "grid", gridTemplateRows: "auto auto", justifyContent: "center" }}>
+                        <p>25</p><p>Completed</p>
+                    </div>
+                    <div className="item_header" style={{ display: "grid", gridTemplateRows: "auto auto", justifyContent: "center" }}>
+                        <p>14</p><p>in progress</p>
+                    </div>
+                    <div className="item_header" style={{ display: "grid", gridTemplateRows: "auto auto", justifyContent: "center" }}>
+                        <p>14</p><p>in progress</p>
+                    </div>
                 </div>
-            </div>
-            <div className="table_header">
-                <div className="item_header" style={{ display: "grid", gridTemplateColumns: "auto auto" }}>
-                    <p>45</p><p>Total Task</p></div>
-                <div className="item_header" style={{ display: "grid", gridTemplateColumns: "auto auto" }}>
-                    <p>25</p><p>Completed</p></div>
-                <div className="item_header" style={{ display: "grid", gridTemplateColumns: "auto auto" }}>
-                    <p>14</p><p>in progress</p></div>
-                <div className="item_header" style={{ display: "grid", gridTemplateColumns: "auto auto" }}>
-                    <p>4</p><p>Overdiew</p></div>
-            </div>
 
-        </div>
-
+            </div>
+        </>
     )
 }
--- src/app/dashboard-manager/page.js
+++ src/app/dashboard-manager/page.js
@@ -1,8 +1,25 @@
 "use client";
 import ProjectOverview from "./ProjectOverview";
+import PieChart from "@/components/PieChart";
 export default function Home() {
-  return (
-      <ProjectOverview />
+      var InputDataExample = {
+    "labels" : ["Andrew H", "Rose S", "Mohammed K", "Tanaka M", "Ming Y"],
+    "dataSetLabel" : "Tasks Assigned",
+    "data" : [7,13,40, 3, 14, 34],
+    "title" : "Employee workload"
+      
 
+
+}
+  return (
+    <div style={{ display: "grid",justifyItems: "center", justifyContent:"center", gridTemplateColumns: "70% 30%"}}>
+       <div style={{ display: "grid",justifyItems: "center", justifyContent:"center", gridTemplateColumns: "auto auto", gap: "5px"}}>
+
+      <ProjectOverview />
+      <ProjectOverview />
+    </div>
+    
+    <PieChart inputData={InputDataExample}/>
+    </div>
   );
 }
