e87e2e5e7d8d25e4adbc866dc4e4cc26ba2142a4
RachelAllen2 <r.l.allen1-24@student.lboro.ac.uk>
2025-10-30 11:45:48 +0000

    [ra] fixed the dates on all profiles

--- src/lib/base-data/projects.js
+++ src/lib/base-data/projects.js
@@ -3,21 +3,31 @@ const baseProjects = [
         id: 0, 
         managerId:0, 
         leaderId: 0,
-        members: [0, 1, 2, 3, 4, 5, 6, 7],
+        members: [0, 2, 4, 5, 6],
         title: "Mobile App", 
         description: "The one and only mobile app",
         creationDate: new Date(2025, 9, 28),
-        dueDate: new Date(2025, 10, 28),
+        dueDate: new Date(2025, 10, 22),
     },
     {
         id: 1, 
-        managerId: 0, 
-        leaderId: 0,
-        members: [0, 1, 2],
+        managerId: 3, 
+        leaderId: 3,
+        members: [2, 3, 4, 6],
         title: "Finance Simulator",
         description: "Finance back-testing",
-        creationDate: new Date(2025, 9, 28),
-        dueDate: new Date(2025, 11, 28)
+        creationDate: new Date(2025, 9, 30),
+        dueDate: new Date(2025, 11, 20)
+    },
+    {
+        id: 2, 
+        managerId: 1, 
+        leaderId: 1,
+        members: [0, 1, 3, 7],
+        title: "Hiring project",
+        description: "A discussion on new hires",
+        creationDate: new Date(2025, 10, 2),
+        dueDate: new Date(2025, 10, 10)
     }
 ]
 
