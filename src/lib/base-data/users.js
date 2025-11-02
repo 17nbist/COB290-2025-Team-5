8c6c370dfbafb5e234e6becb12012aecbbc8edc9
David Oladiran <daveanddan70@gmail.com>
2025-11-02 16:21:03 +0000

    [do] Update emails

--- src/lib/base-data/users.js
+++ src/lib/base-data/users.js
@@ -1,62 +1,62 @@
 //In the next phrase, we need to remove the profilPic and passwords or have hashes of them.
 const baseUsers = [
     {
-        id: 0, 
-        email: "manager1@gmail.com", 
-        name: "John Doe", 
+        id: 0,
+        email: "manager1@make-it-all.co.uk",
+        name: "John Doe",
         role: "Manager",
-        profilePic:"/images/pexels-blitzboy-1040880.jpg", 
+        profilePic:"/images/pexels-blitzboy-1040880.jpg",
         password: "password123"
     },
     {
-        id: 1, 
-        email: "ceo@gmail.com", 
+        id: 1,
+        email: "ceo@make-it-all.co.uk",
         name: "Mike Oxlong", 
         role: "CEO",
-        profilePic:"/images/pexels-moh-adbelghaffar-771742.jpg", 
+        profilePic:"/images/pexels-moh-adbelghaffar-771742.jpg",
         password: "password123"
     },
     {
-        id: 2, 
-        email: "employee1@gmail.com", 
-        name: "Gabe Itch", 
-        role: "Employee", 
+        id: 2,
+        email: "employee1@make-it-all.co.uk",
+        name: "Gabe Itch",
+        role: "Employee",
         profilePic:"/images/pexels-rfera-432059.jpg",
         password: "password123"
     },
     {
-        id: 3, 
-        email: "manager2@gmail.com", 
-        name: "Sarah Connor", 
-        role: "Manager", 
+        id: 3,
+        email: "manager2@make-it-all.co.uk",
+        name: "Sarah Connor",
+        role: "Manager",
         password: "password123"
     },
     {
-        id: 4, 
-        email: "employee2@gmail.com", 
-        name: "Ben Dover", 
-        role: "Employee", 
-        password: "password123"
-    },
-    {
-        id: 5, 
-        email: "employee3@gmail.com", 
-        name: "Anita Bath", 
+        id: 4,
+        email: "employee2@make-it-all.co.uk",
+        name: "Ben Dover",
         role: "Employee",
         password: "password123"
     },
     {
-        id: 6, 
-        email: "employee4@gmail.com", 
-        name: "Phil McCracken", 
-        role: "Employee", 
+        id: 5,
+        email: "employee3@make-it-all.co.uk",
+        name: "Anita Bath",
+        role: "Employee",
         password: "password123"
     },
     {
-        id: 7, 
-        email: "hr1@gmail.com", 
-        name: "Karen Price", 
-        role: "HR", 
+        id: 6,
+        email: "employee4@make-it-all.co.uk",
+        name: "Phil McCracken",
+        role: "Employee",
+        password: "password123"
+    },
+    {
+        id: 7,
+        email: "hr1@make-it-all.co.uk",
+        name: "Karen Price",
+        role: "HR",
         password: "password123"
     }
 ]
